f748355efe2e2771774e51d4b7f24d0d291f0bf5
Anton Khorev <tony29@yandex.ru>
2023-09-14 03:55:20 +0300

    Move common pagination code to mixin

--- app/controllers/changeset_comments_controller.rb
+++ app/controllers/changeset_comments_controller.rb
@@ -1,5 +1,6 @@
 class ChangesetCommentsController < ApplicationController
   include UserMethods
+  include PaginationMethods
 
   layout "site", :except => :rss
 
@@ -49,26 +50,6 @@ class ChangesetCommentsController < ApplicationController
   end
 
   private
-
-  def get_page_items(items, includes)
-    id_column = "#{items.table_name}.id"
-    page_items = if params[:before]
-                   items.where("#{id_column} < ?", params[:before]).order(:id => :desc)
-                 elsif params[:after]
-                   items.where("#{id_column} > ?", params[:after]).order(:id => :asc)
-                 else
-                   items.order(:id => :desc)
-                 end
-
-    page_items = page_items.limit(20)
-    page_items = page_items.includes(includes)
-    page_items = page_items.sort.reverse
-
-    newer_items_id = page_items.first.id if page_items.count.positive? && items.exists?(["#{id_column} > ?", page_items.first.id])
-    older_items_id = page_items.last.id if page_items.count.positive? && items.exists?(["#{id_column} < ?", page_items.last.id])
-
-    [page_items, newer_items_id, older_items_id]
-  end
 
   ##
   # Get the maximum number of comments to return
