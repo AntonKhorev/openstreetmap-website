19af9b5b1099decc24e056cb8fb3324f8277c7e9
Anton Khorev <tony29@yandex.ru>
2023-10-21 20:39:55 +0300

    Highlight moderator-only scopes when showing authorizations

---
+++ app/helpers/authorization_helper.rb
@@ -0,0 +1,15 @@
+module AuthorizationHelper
+  include ActionView::Helpers::TranslationHelper
+
+  MODERATOR_SCOPES = %w[].freeze
+
+  def authorization_scope(scope)
+    html = []
+    if MODERATOR_SCOPES.include? scope
+      html << image_tag("roles/moderator.png", :srcset => image_path("roles/moderator.svg", :class => "align-text-bottom"), :size => "20x20")
+      html << " "
+    end
+    html << t("oauth.scopes.#{scope}")
+    safe_join(html)
+  end
+end
