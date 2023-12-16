0a63b9422aff6a0b852f4ea5f88a7d5c4b1a64b2
Anton Khorev <tony29@yandex.ru>
2023-12-16 18:18:17 +0300

    Allow to specify stroke direction in map key data

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -1,12 +1,16 @@
 module SvgHelper
   def svg_image_tag(width, height, **rect_attrs)
-    stroke_width = rect_attrs[:stroke] ? 1 : 0
-    inset = 0.5 * stroke_width if stroke_width.positive?
+    stroke_side_x = rect_attrs["stroke-direction"] == "horizontal" ? -1 : 1
+    stroke_side_y = rect_attrs["stroke-direction"] == "vertical" ? -1 : 1
+    rect_attrs = rect_attrs.except("stroke-direction")
+    stroke_width = rect_attrs["stroke"] ? 1 : 0
+    inset_x = 0.5 * stroke_side_x * stroke_width if stroke_width.positive?
+    inset_y = 0.5 * stroke_side_y * stroke_width if stroke_width.positive?
     rect_geometry_attrs = {
-      :x => inset,
-      :y => inset,
-      :width => width - stroke_width,
-      :height => height - stroke_width
+      :x => inset_x,
+      :y => inset_y,
+      :width => width - (stroke_side_x * stroke_width),
+      :height => height - (stroke_side_y * stroke_width)
     }
     rect = tag.rect(**rect_geometry_attrs, **rect_attrs)
     svg = tag.svg rect, :xmlns => "http://www.w3.org/2000/svg", :width => width, :height => height
