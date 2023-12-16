f9203f3050bb397852d33f03a8753a2d8acec406
Anton Khorev <tony29@yandex.ru>
2023-12-16 16:55:04 +0300

    Allow stroke attribute in map key data

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -1,8 +1,15 @@
 module SvgHelper
-  def solid_svg_image_tag(width, height, fill)
-    svg = u("<svg xmlns='http://www.w3.org/2000/svg' width='#{width}' height='#{height}'>" \
-            "<rect width='100%' height='100%' fill='#{fill}' />" \
-            "</svg>")
-    image_tag "data:image/svg+xml,#{svg}"
+  def svg_image_tag(width, height, **rect_attrs)
+    stroke_width = rect_attrs[:stroke] ? 1 : 0
+    inset = 0.5 * stroke_width if stroke_width.positive?
+    rect_geometry_attrs = {
+      :x => inset,
+      :y => inset,
+      :width => width - stroke_width,
+      :height => height - stroke_width
+    }
+    rect = tag.rect(**rect_geometry_attrs, **rect_attrs)
+    svg = tag.svg rect, :xmlns => "http://www.w3.org/2000/svg", :width => width, :height => height
+    image_tag "data:image/svg+xml,#{u(svg)}"
   end
 end
