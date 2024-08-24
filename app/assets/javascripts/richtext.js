6c9c6892dbb3ee1160ea6d395861376464c96f0a
Anton Khorev <tony29@yandex.ru>
2024-08-24 17:32:49 +0300

    WIP

--- app/assets/javascripts/richtext.js
+++ app/assets/javascripts/richtext.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
+  console.log("doc ready"); ///
+
   /*
    * When the text in an edit pane is changed, clear the contents of
    * the associated preview pne so that it will be regenerated when
    * the user next switches to it.
    */
-  $(document).on("change", ".richtext_container textarea", function () {
+  $("#content").on("change", ".richtext_container textarea", function () {
     var container = $(this).closest(".richtext_container");
 
     container.find(".tab-pane[id$='_preview']").empty();
-  }).on("invalid", ".richtext_container textarea", function () {
+  });
+  
+  $("#content").on("invalid", ".richtext_container textarea", function () {
+  // $(".richtext_container textarea").on("invalid", function () {
     var container = $(this).closest(".richtext_container");
+    console.log("invalid", container.find("button[data-bs-target$='_edit']"));
 
     container.find("button[data-bs-target$='_edit']").tab("show");
   });
@@ -18,7 +24,7 @@ $(document).ready(function () {
    * Install a handler to set the minimum preview pane height
    * when switching away from an edit pane
    */
-  $(document).on("hide.bs.tab", ".richtext_container button[data-bs-target$='_edit']", function () {
+  $("#content").on("hide.bs.tab", ".richtext_container button[data-bs-target$='_edit']", function () {
     var container = $(this).closest(".richtext_container");
     var editor = container.find("textarea");
     var preview = container.find(".tab-pane[id$='_preview']");
@@ -30,7 +36,7 @@ $(document).ready(function () {
   /*
    * Install a handler to switch to preview mode
    */
-  $(document).on("show.bs.tab", ".richtext_container button[data-bs-target$='_preview']", function () {
+  $("#content").on("show.bs.tab", ".richtext_container button[data-bs-target$='_preview']", function () {
     var container = $(this).closest(".richtext_container");
     var editor = container.find("textarea");
     var preview = container.find(".tab-pane[id$='_preview']");
@@ -63,6 +69,11 @@ $(document).ready(function () {
     });
   };
 
+  // $(document).on("turbo:load", updateHelp);
   updateHelp();
+  $(document).on("turbo:load", function () {
+    console.log("doc turbo load inner"); ///
+    updateHelp();
+  });
   $(window).on("resize", updateHelp);
 });
