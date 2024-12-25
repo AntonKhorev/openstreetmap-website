355d20853e9b3e47557a63ee8f7ffb055a0e68b3
Anton Khorev <tony29@yandex.ru>
2024-12-25 03:38:30 +0300

    Rename updateMarker() to addCreatedNoteMarker()
    This function doesn't update any existing marker.

--- app/assets/javascripts/index/new_note.js
+++ app/assets/javascripts/index/new_note.js
@@ -59,7 +59,7 @@ OSM.NewNote = function (map) {
 
     function noteCreated(feature) {
       content.find("textarea").val("");
-      updateMarker(feature);
+      addCreatedNoteMarker(feature);
       noteLayer.removeLayer(newNoteMarker);
       newNoteMarker = null;
       addNoteButton.removeClass("active");
@@ -67,7 +67,7 @@ OSM.NewNote = function (map) {
     }
   }
 
-  function updateMarker(feature) {
+  function addCreatedNoteMarker(feature) {
     var marker = L.marker(feature.geometry.coordinates.reverse(), {
       icon: noteIcons[feature.properties.status],
       opacity: 0.9,
