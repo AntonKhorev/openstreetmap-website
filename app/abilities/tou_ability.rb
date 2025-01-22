6ba7fa84d071b22377d22fcc3d414d1363948a54
Anton Khorev <tony29@yandex.ru>
2025-01-22 07:59:36 +0300

    Remove read_comments ability from changesets when restricted

--- app/abilities/tou_ability.rb
+++ app/abilities/tou_ability.rb
@@ -5,5 +5,9 @@ class TouAbility
 
   def initialize
     can :read_comments, Changeset
+
+    Settings.data_restrictions.each do |restriction|
+      cannot :read_comments, Changeset if restriction.type == :hide_changeset_comments
+    end
   end
 end
--- test/abilities/tou_ability_test.rb
+++ test/abilities/tou_ability_test.rb
@@ -8,4 +8,12 @@ class TouAbilityTest < ActiveSupport::TestCase
 
     assert ability.can?(:read_comments, Changeset)
   end
+
+  test "show map users with a restriction" do
+    with_settings(:data_restrictions => [{ :type => :hide_changeset_comments }]) do
+      ability = TouAbility.new
+
+      assert ability.cannot?(:read_comments, Changeset)
+    end
+  end
 end
