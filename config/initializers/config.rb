631c7e3c548b568210e4d405dd8c9a34db1e3612
Anton Khorev <tony29@yandex.ru>
2025-01-22 07:43:32 +0300

    Define hide_changeset_comments restriction

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -86,5 +86,8 @@ Config.setup do |config|
     required(:trace_file_storage).filled(:str?)
     required(:trace_image_storage).filled(:str?)
     required(:trace_icon_storage).filled(:str?)
+    required(:data_restrictions).array(:hash) do
+      required(:type).value(:included_in? => [:hide_changeset_comments])
+    end
   end
 end
