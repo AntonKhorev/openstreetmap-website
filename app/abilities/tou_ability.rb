b53ddfb0d7bb3a46fe58a9ec128385884b46fd05
Anton Khorev <tony29@yandex.ru>
2025-01-28 20:00:36 +0300

    Define restriction requiring ToU acceptance

--- app/abilities/tou_ability.rb
+++ app/abilities/tou_ability.rb
@@ -3,13 +3,14 @@
 class TouAbility
   include CanCan::Ability
 
-  def initialize(_user)
+  def initialize(user)
     now = Time.zone.now
     Settings.data_restrictions.each do |restriction|
       subject = ChangesetComment if restriction.type == :hide_changeset_comments
 
       next if subject.nil?
       next if restriction.activates_on && restriction.activates_on > now
+      next if restriction.unless_tou_accepted && user&.tou_agreed
 
       cannot :manage, subject
     end
