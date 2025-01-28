65134f4e8b4bbd5493d3f2c808059da0d08dfc1d
Anton Khorev <tony29@yandex.ru>
2025-01-28 20:00:36 +0300

    Define restriction requiring ToU acceptance

--- test/abilities/tou_ability_test.rb
+++ test/abilities/tou_ability_test.rb
@@ -50,4 +50,55 @@ class TouAbilityTest < ActiveSupport::TestCase
       end
     end
   end
+
+  test "read changeset comments with a restriction that requires accepting ToU while not logged in " do
+    with_settings(:data_restrictions => [{ :type => :hide_changeset_comments,
+                                           :unless_tou_accepted => true }]) do
+      ability = PermittingAbility.new.merge(TouAbility.new(nil))
+
+      [:index, :show].each do |act|
+        assert ability.cannot? act, ChangesetComment
+      end
+    end
+  end
+
+  test "read changeset comments with a restriction that requires accepting ToU while not accepted ToU" do
+    user = create(:user, :tou_agreed => nil)
+
+    with_settings(:data_restrictions => [{ :type => :hide_changeset_comments,
+                                           :unless_tou_accepted => true }]) do
+      ability = PermittingAbility.new.merge(TouAbility.new(user))
+
+      [:index, :show].each do |act|
+        assert ability.cannot? act, ChangesetComment
+      end
+    end
+  end
+
+  test "read changeset comments with a restriction that requires accepting ToU while accepted ToU" do
+    user = create(:user, :tou_agreed => 10.hours.ago)
+
+    with_settings(:data_restrictions => [{ :type => :hide_changeset_comments,
+                                           :unless_tou_accepted => true }]) do
+      ability = PermittingAbility.new.merge(TouAbility.new(user))
+
+      [:index, :show].each do |act|
+        assert ability.can? act, ChangesetComment
+      end
+    end
+  end
+
+  test "read changeset comments with a restriction that activates in the past and requires accepting ToU while accepted ToU" do
+    user = create(:user, :tou_agreed => 10.hours.ago)
+
+    with_settings(:data_restrictions => [{ :type => :hide_changeset_comments,
+                                           :activates_on => 20.hours.ago,
+                                           :unless_tou_accepted => true }]) do
+      ability = PermittingAbility.new.merge(TouAbility.new(user))
+
+      [:index, :show].each do |act|
+        assert ability.can? act, ChangesetComment
+      end
+    end
+  end
 end
