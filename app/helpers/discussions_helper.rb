bb8f62ec50f6b874d9eaeb56a4d3bcae1b24b04b
Anton Khorev <tony29@yandex.ru>
2025-02-07 20:17:49 +0300

    Skip discussion comments if they are far enough from selected

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,9 +1,44 @@
 module DiscussionsHelper
+  CONTEXT_SIZE = 3
+
   def discussions_with_selected_comments(context_comments, selected_comment_ids, discussion_key)
-    context_comments.each_with_object({}) do |comment, discussions|
+    all_comments_collected_for_discussions = context_comments.each_with_object({}) do |comment, all_comments|
       discussion_id = comment[discussion_key]
-      discussions[discussion_id] ||= []
-      discussions[discussion_id] << { :comment => comment, :selected => selected_comment_ids.include?(comment[:id]) }
+      all_comments[discussion_id] ||= []
+      all_comments[discussion_id] << comment
+    end
+
+    all_comments_collected_for_discussions.transform_values do |all_comments|
+      discussion_items = []
+      skipped_before = false
+      can_keep_after = 0
+      context_buffer = []
+
+      all_comments.each do |comment|
+        if selected_comment_ids.include?(comment[:id])
+          if skipped_before
+            discussion_items << {}
+            skipped_before = false
+          end
+          discussion_items.concat(context_buffer)
+          context_buffer = []
+          discussion_items << { :comment => comment, :selected => true }
+          can_keep_after = CONTEXT_SIZE
+        elsif can_keep_after.positive?
+          discussion_items << { :comment => comment, :selected => false }
+          can_keep_after -= 1
+        else
+          context_buffer << { :comment => comment, :selected => false }
+          if context_buffer.size > CONTEXT_SIZE
+            skipped_before = true
+            context_buffer.shift
+          end
+        end
+      end
+
+      discussion_items << {} if context_buffer.size.positive?
+
+      discussion_items
     end
   end
 end
--- test/helpers/discussions_helper_test.rb
+++ test/helpers/discussions_helper_test.rb
@@ -55,4 +55,76 @@ class DiscussionsHelperTest < ActionView::TestCase
                                     { :comment => { :id => 41, :discussion_id => 12, :text => "test 41" }, :selected => false }] }
     assert_equal expected_discussions, discussions
   end
+
+  def test_discussion_with_short_context_before_selected_comment
+    context_comments = [{ :id => 153, :discussion_id => 23 },
+                        { :id => 152, :discussion_id => 23 },
+                        { :id => 151, :discussion_id => 23 },
+                        { :id => 150, :discussion_id => 23 }]
+    selected_comment_ids = [150]
+
+    discussions = discussions_with_selected_comments(context_comments, selected_comment_ids, :discussion_id)
+
+    assert_equal [23], discussions.keys
+    expected_discussions = { 23 => [{ :comment => { :id => 153, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 152, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 151, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 150, :discussion_id => 23 }, :selected => true }] }
+    assert_equal expected_discussions, discussions
+  end
+
+  def test_discussion_with_long_context_before_selected_comment
+    context_comments = [{ :id => 154, :discussion_id => 23 },
+                        { :id => 153, :discussion_id => 23 },
+                        { :id => 152, :discussion_id => 23 },
+                        { :id => 151, :discussion_id => 23 },
+                        { :id => 150, :discussion_id => 23 }]
+    selected_comment_ids = [150]
+
+    discussions = discussions_with_selected_comments(context_comments, selected_comment_ids, :discussion_id)
+
+    assert_equal [23], discussions.keys
+    expected_discussions = { 23 => [{},
+                                    { :comment => { :id => 153, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 152, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 151, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 150, :discussion_id => 23 }, :selected => true }] }
+    assert_equal expected_discussions, discussions
+  end
+
+  def test_discussion_with_short_context_after_selected_comment
+    context_comments = [{ :id => 153, :discussion_id => 23 },
+                        { :id => 152, :discussion_id => 23 },
+                        { :id => 151, :discussion_id => 23 },
+                        { :id => 150, :discussion_id => 23 }]
+    selected_comment_ids = [153]
+
+    discussions = discussions_with_selected_comments(context_comments, selected_comment_ids, :discussion_id)
+
+    assert_equal [23], discussions.keys
+    expected_discussions = { 23 => [{ :comment => { :id => 153, :discussion_id => 23 }, :selected => true },
+                                    { :comment => { :id => 152, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 151, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 150, :discussion_id => 23 }, :selected => false }] }
+    assert_equal expected_discussions, discussions
+  end
+
+  def test_discussion_with_long_context_after_selected_comment
+    context_comments = [{ :id => 154, :discussion_id => 23 },
+                        { :id => 153, :discussion_id => 23 },
+                        { :id => 152, :discussion_id => 23 },
+                        { :id => 151, :discussion_id => 23 },
+                        { :id => 150, :discussion_id => 23 }]
+    selected_comment_ids = [154]
+
+    discussions = discussions_with_selected_comments(context_comments, selected_comment_ids, :discussion_id)
+
+    assert_equal [23], discussions.keys
+    expected_discussions = { 23 => [{ :comment => { :id => 154, :discussion_id => 23 }, :selected => true },
+                                    { :comment => { :id => 153, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 152, :discussion_id => 23 }, :selected => false },
+                                    { :comment => { :id => 151, :discussion_id => 23 }, :selected => false },
+                                    {}] }
+    assert_equal expected_discussions, discussions
+  end
 end
